ae53fd8ed8faeb26d97c0c5430b57889636ce97f
DawsonG5 <105284996+DawsonG5@users.noreply.github.com>
2023-03-14 08:58:16 -0300

    Add files via upload

---
+++ ProjetoBiblioteca/main.c
@@ -0,0 +1,12 @@
+
+int max (int num1, int num2){
+	
+	/* declaração de variável local */
+	int resultado;
+	
+	if (num1> num2)
+		resultado = num1;
+	else
+		resultado = num2;
+	return resultado;
+}
